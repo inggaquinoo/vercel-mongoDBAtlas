bc7698e75b21c172f6e244cf4ab78245b01b37f7
Gustavo <gustavo@gmail.com>
2022-12-02 12:45:20 -0500

    ok

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const express = require("express")
-const mongoose = require("mongoose")
-
+const express = require('express')
+const mongoose = require('mongoose')
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+//CORS: Nos permite hacer solicitudes desde un origen diferente
+const cors = require('cors');
 
-app.get("*",(req, res)=>{
+app.use(cors());
+
+app.use(express.json());
+
+
+app.get("/",(req, res)=>{
     res.send("Hola Tavo, tú estas conectado exitosamente a MongoDB")
 });
+
+require('dotenv').config();
+
+//Se lee: sino tenemos el puerto en nuestro archivo .env
+//el puerto se establecerá en 3000
+const port = process.env.PORT || 3000;
+
+//Se reemplazan los valores por las variables de entorno las cuales se encuentran
+//en el archivo .env
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.eyoebix.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{useNewUrlParser: true, useUnifiedTopology: true})
+    //Si conecta sin problemas
+    .then(result => {
+    //se usa comilla invertida para poder introducir un valor dinámico
+    app.listen(port, () => console.log(`Server is running on PORT ${port}`));
+    })
+    //Si tiene problemas al conectar 
+    .catch(err => console.log(err))
 
 module.exports = app;
